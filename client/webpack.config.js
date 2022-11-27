d6bee56b3174a568969682f71cb58bf9f22c4a20
LaLacou <108777796+llacourciere@users.noreply.github.com>
2022-11-26 23:08:43 -0800

    updated package.json

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
         filename: "bundle.js",
         path: path.resolve(__dirname, "dist"),
     },
-    mode: "development",
+    mode: "production",
     plugins: [
       new InjectManifest({
         swSrc: './src/sw.js',
